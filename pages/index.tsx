e40d98814a36c16562cdf190bd8732e961c2ebdb
bebeshannu <shainajyothica27@gmail.com>
2023-03-31 16:31:06 +0530

    second commit- navrbar added

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+
+import {getSession, signOut} from "next-auth/react"
+import { NextPageContext } from "next";
+import Navbar from "@/components/Navbar";
+
+export async function getServerSideProps(context: NextPageContext){
+  const session = await getSession(context);
+
+  if(!session){
+    
+  return {
+  redirect: {
+    destination: '/auth',
+    permanent: false,
+  }
+ }
+}
+  return {
+    props: {}
+  }
+}
+
+export default function Home() {
+  
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
